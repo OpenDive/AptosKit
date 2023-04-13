ee9e87ea78ba55d955b48cc3d67b02851ef1b327
Marcus Arnett <Marcodotiobusiness@gmail.com>
2023-04-13 10:57:02 -0400

    Implement Multisig Init, Close #29

--- Sources/AptosKit/Utils/ed25519/MultiSignature.swift
+++ Sources/AptosKit/Utils/ed25519/MultiSignature.swift
@@ -10,4 +10,19 @@ import Foundation
 public struct MultiSignature {
     public var signatures: [Signature]
     public var bitmap: Data
+    
+    init(publicKey: MultiPublicKey, signatureMap: [(PublicKey, Signature)]) {
+        self.signatures = []
+        var bitmap = 0
+        
+        for entry in signatureMap {
+            self.signatures.append(entry.1)
+            if let index = publicKey.keys.firstIndex(of: entry.0) {
+                let shift = 31 - index
+                bitmap = bitmap | (1 << shift)
+            }
+        }
+        
+        self.bitmap = withUnsafeBytes(of: bitmap) { Data($0) }
+    }
 }
