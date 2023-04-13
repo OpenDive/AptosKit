5d406d02518e82ce798ec8394c54d95745d3939a
Marcus Arnett <Marcodotiobusiness@gmail.com>
2023-04-13 09:56:43 -0400

    Implement MultiPublicKey fromBytes Function, Close #27

--- Sources/AptosKit/Utils/ed25519/MultiPublicKey.swift
+++ Sources/AptosKit/Utils/ed25519/MultiPublicKey.swift
@@ -11,17 +11,17 @@ public struct MultiPublicKey: CustomStringConvertible {
     public var keys: [PublicKey]
     public var threshold: Int
     
-    public let minKeys: Int = 2
-    public let maxKeys: Int = 32
-    public let minThreshold: Int = 1
+    public static let minKeys: Int = 2
+    public static let maxKeys: Int = 32
+    public static let minThreshold: Int = 1
     
     init(keys: [PublicKey], threshold: Int, checked: Bool = true) throws {
         if checked {
-            if self.minKeys >= keys.count || self.maxKeys <= keys.count {
-                throw NSError(domain: "Must have between \(self.minKeys) and \(self.maxKeys) keys.", code: -1)
+            if MultiPublicKey.minKeys > keys.count || MultiPublicKey.maxKeys < keys.count {
+                throw NSError(domain: "Must have between \(MultiPublicKey.minKeys) and \(MultiPublicKey.maxKeys) keys.", code: -1)
             }
-            if self.minThreshold >= threshold || threshold >= keys.count {
-                throw NSError(domain: "Threshold must be between \(self.minThreshold) and \(keys.count).", code: -1)
+            if MultiPublicKey.minThreshold > threshold || threshold > keys.count {
+                throw NSError(domain: "Threshold must be between \(MultiPublicKey.minThreshold) and \(keys.count).", code: -1)
             }
         }
         
@@ -39,5 +39,33 @@ public struct MultiPublicKey: CustomStringConvertible {
             concatenatedKeys += key.key
         }
         return concatenatedKeys + Data([UInt8(self.threshold)])
+    }
+    
+    public static func fromBytes(_ key: Data) throws -> MultiPublicKey {
+        let minKeys = MultiPublicKey.minKeys
+        let maxKeys = MultiPublicKey.maxKeys
+        let minThreshold = MultiPublicKey.minThreshold
+        
+        let nSigners = Int(key.count / PublicKey.LENGTH)
+        
+        if minKeys > nSigners || nSigners > maxKeys {
+            throw NSError(domain: "Must have between \(minKeys) and \(maxKeys) keys.", code: -1)
+        }
+        
+        let threshold = Int(key[-1])
+        
+        if minThreshold > threshold || threshold > nSigners {
+            throw NSError(domain: "Threshold must be between \(minThreshold) and \(nSigners).", code: -1)
+        }
+        
+        var keys: [PublicKey] = []
+        
+        for i in 0..<nSigners {
+            let startByte = i * PublicKey.LENGTH
+            let endByte = (i + 1) * PublicKey.LENGTH
+            keys.append(try PublicKey(data: Data(key[startByte..<endByte])))
+        }
+        
+        return try MultiPublicKey(keys: keys, threshold: threshold)
     }
 }
