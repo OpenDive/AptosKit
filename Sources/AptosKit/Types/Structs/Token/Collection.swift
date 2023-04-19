f03e95c0ac7dd3aef4d9d4b228591145ae0edfa5
Marcus Arnett <Marcodotiobusiness@gmail.com>
2023-04-19 13:46:15 -0400

    Implement Object Type, Close #89

---
+++ Sources/AptosKit/Types/Structs/Token/Collection.swift
@@ -0,0 +1,55 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marcus Arnett on 4/19/23.
+//
+
+import Foundation
+
+public struct Collection: CustomStringConvertible {
+    let creator: AccountAddress
+    let _description: String
+    let name: String
+    let uri: String
+    
+    let structTag: String = "0x4::collection::Collection"
+    
+    public init(
+        creator: AccountAddress,
+        _description: String,
+        name: String,
+        uri: String
+    ) {
+        self.creator = creator
+        self._description = _description
+        self.name = name
+        self.uri = uri
+    }
+    
+    public var description: String {
+        return "AccountAddress[creator: \(self.creator), description: \(self._description), name: \(self.name), uri: \(self.uri)]"
+    }
+    
+    public static func parse(_ resource: [String: Any]) throws -> Collection {
+        guard let creator = resource["creator"], creator is String else {
+            throw NSError(domain: "Invalid resource", code: -1)
+        }
+        guard let description = resource["description"], description is String else {
+            throw NSError(domain: "Invalid resource", code: -1)
+        }
+        guard let name = resource["name"], name is String else {
+            throw NSError(domain: "Invalid resource", code: -1)
+        }
+        guard let uri = resource["uri"], uri is String else {
+            throw NSError(domain: "Invalid resource", code: -1)
+        }
+        
+        return Collection(
+            creator: try AccountAddress.fromHex(creator as! String),
+            _description: description as! String,
+            name: name as! String,
+            uri: uri as! String
+        )
+    }
+}
--- Sources/AptosKit/Types/Structs/Token/Object.swift
+++ Sources/AptosKit/Types/Structs/Token/Object.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 public struct Object: CustomStringConvertible {
-    let allowedUngatedTransfer: Bool
+    let allowUngatedTransfer: Bool
     let owner: AccountAddress
     
-    public init(allowedUngatedTransfer: Bool, owner: AccountAddress) {
-        self.allowedUngatedTransfer = allowedUngatedTransfer
+    public init(allowUngatedTransfer: Bool, owner: AccountAddress) {
+        self.allowUngatedTransfer = allowUngatedTransfer
         self.owner = owner
     }
     
     public var description: String {
-        return ", owner: \(self.owner.description)"
+        return "Object[allow_ungated_transfer: \(self.allowUngatedTransfer), owner: \(self.owner.description)]"
     }
     
-    public func parse(_ resource: [String: Any]) throws -> Object {
+    public static func parse(_ resource: [String: Any]) throws -> Object {
         guard let allowUngatedTranser = resource["allow_untaged_transfer"], allowUngatedTranser is Bool else {
             throw NSError(domain: "Invalid resource", code: -1)
         }
@@ -28,7 +28,7 @@ public struct Object: CustomStringConvertible {
             throw NSError(domain: "Invalid resource", code: -1)
         }
         return Object(
-            allowedUngatedTransfer: allowUngatedTranser as! Bool,
+            allowUngatedTransfer: allowUngatedTranser as! Bool,
             owner: try AccountAddress.fromHex(accountAddress as! String)
         )
     }
