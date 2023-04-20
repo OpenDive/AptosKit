d37f4be899d8a639f4a10d5d382685dec361f1b6
Marcus Arnett <Marcodotiobusiness@gmail.com>
2023-04-20 14:00:18 -0400

    Implement BIP39 Wallet, Close #98

---
+++ Sources/AptosKit/Types/Structs/Wallet.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marcus Arnett on 4/20/23.
+//
+
+import Foundation
+
+public struct Wallet {
+    public let passphrase: [String]
+    public let account: Account
+    
+    public init(passphrase: [String], account: Account) {
+        self.passphrase = passphrase
+        self.account = account
+    }
+    
+    public init() throws {
+        self.account = try Account.generate()
+        self.passphrase = try Mnemonic.toMnemonic([UInt8](self.account.privateKey.key))
+    }
+    
+    public init(account: Account) throws {
+        self.account = account
+        self.passphrase = try Mnemonic.toMnemonic([UInt8](self.account.privateKey.key))
+    }
+    
+    public init(passphrase: [String]) throws {
+        self.account = try Account.loadKey(Data(try Mnemonic.toEntropy(passphrase)).hexEncodedString())
+        self.passphrase = passphrase
+    }
+}
