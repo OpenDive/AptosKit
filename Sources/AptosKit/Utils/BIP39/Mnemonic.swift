752f2254beb958df92be9d258ad84be6d4eb35e3
Marcus Arnett <Marcodotiobusiness@gmail.com>
2023-04-22 12:26:32 -0400

    Write documentation for BIP39 Mnemonic class, Close #102

--- Sources/AptosKit/Utils/BIP39/Mnemonic.swift
+++ Sources/AptosKit/Utils/BIP39/Mnemonic.swift
@@ -1,38 +1,59 @@
 //
 //  Mnemonic.swift
+//  AptosKit
 //
-//  See BIP39 specification for more info:
-//  https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
+//  Copyright (c) 2023 OpenDive
 //
-//  Created by Liu Pengpeng on 2019/10/10.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 import CryptoSwift
 
+/// Used for deriving BIP39 Phrases
 public class Mnemonic {
     public enum Error: Swift.Error {
         case invalidMnemonic
         case invalidEntropy
     }
-    
+
+    /// Contains the seed phrases themselves
     public let phrase: [String]
+
+    /// Used for storing the user's passphrase
     let passphrase: String
 
     public init(strength: Int = 256, wordlist: [String] = Wordlists.english) {
         precondition(strength % 32 == 0, "Invalid entropy")
 
-        // 1.Random Bytes
+        // Generates random bytes to use as entropy for the mnemonic phrase.
         var bytes = [UInt8](repeating: 0, count: strength / 8)
         _ = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
 
-        // 2.Entropy -> Mnemonic
+        // Converts the bytes into a mnemonic phrase by adding a checksum and converting the resulting binary string into words using the specified wordlist.
         let entropyBits = String(bytes.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
         let checksumBits = Mnemonic.deriveChecksumBits(bytes)
         let bits = entropyBits + checksumBits
 
         var phrase = [String]()
         for i in 0 ..< (bits.count / 11) {
+            // Extracts each 11-bit index from the binary string and converts it into a word using the specified wordlist.
             let wi = Int(
                 bits[bits.index(bits.startIndex, offsetBy: i * 11) ..< bits
                     .index(bits.startIndex, offsetBy: (i + 1) * 11)],
@@ -41,34 +62,38 @@ public class Mnemonic {
             phrase.append(String(wordlist[wi]))
         }
 
+        // Assigns the generated mnemonic phrase to the instance variable `phrase` and sets `passphrase` to an empty string.
         self.phrase = phrase
         passphrase = ""
     }
-    
+
     public init(wordcount: Int = 12, wordlist: [String] = Wordlists.english) {
         var strength: Int = 128
-        
+
         switch wordcount {
+            // If wordcount is 15, sets strength to 160 bits. If wordcount is 18, sets strength to 192 bits. If wordcount is 21, sets strength to 224 bits. Otherwise, sets strength to 128 bits.
             case 15: strength = 160
             case 18: strength = 192
             case 21: strength = 224
             default: strength = 128
         }
-        
+
+        // If wordcount is less than 12, sets strength to 128 bits. If wordcount is greater than 24, sets strength to 256 bits.
         if wordcount < 12 { strength = 128 }
         if wordcount > 24 { strength = 256 }
 
-        // 1.Random Bytes
+        // Generates random bytes to use as entropy for the mnemonic phrase.
         var bytes = [UInt8](repeating: 0, count: strength / 8)
         _ = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
 
-        // 2.Entropy -> Mnemonic
+        // Converts the bytes into a mnemonic phrase by adding a checksum and converting the resulting binary string into words using the specified wordlist.
         let entropyBits = String(bytes.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
         let checksumBits = Mnemonic.deriveChecksumBits(bytes)
         let bits = entropyBits + checksumBits
 
         var phrase = [String]()
         for i in 0 ..< (bits.count / 11) {
+            // Extracts each 11-bit index from the binary string and converts it into a word using the specified wordlist.
             let wi = Int(
                 bits[bits.index(bits.startIndex, offsetBy: i * 11) ..< bits
                     .index(bits.startIndex, offsetBy: (i + 1) * 11)],
@@ -77,6 +102,7 @@ public class Mnemonic {
             phrase.append(String(wordlist[wi]))
         }
 
+        // Assigns the generated mnemonic phrase to the instance variable `phrase` and sets `passphrase` to an empty string.
         self.phrase = phrase
         passphrase = ""
     }
@@ -94,7 +120,15 @@ public class Mnemonic {
         self.passphrase = passphrase
     }
 
-    // Entropy -> Mnemonic
+    /// Converts an array of bytes to a list of mnemonic words using a specified wordlist.
+    ///
+    /// - Parameters:
+    ///    - bytes: An array of bytes to be converted to a mnemonic phrase.
+    ///    - wordlist: An optional parameter specifying the wordlist to be used. If not provided, the default English wordlist will be used.
+    ///
+    /// - Returns: A list of mnemonic words representing the input bytes.
+    ///
+    /// - Throws: Any error thrown by Mnemonic.deriveChecksumBits() function if there is an issue calculating the checksum bits.
     public static func toMnemonic(_ bytes: [UInt8], wordlist: [String] = Wordlists.english) throws -> [String] {
         let entropyBits = String(bytes.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
         let checksumBits = Mnemonic.deriveChecksumBits(bytes)
@@ -112,7 +146,15 @@ public class Mnemonic {
         return phrase
     }
 
-    // Mnemonic -> Entropy
+    /// Converts a list of mnemonic words to an array of bytes.
+    ///
+    /// - Parameters:
+    ///    - phrase: A list of mnemonic words to be converted to an array of bytes.
+    ///    - wordlist: An optional parameter specifying the wordlist to be used. If not provided, the default English wordlist will be used.
+    ///
+    /// - Returns: An array of bytes representing the input mnemonic phrase.
+    ///
+    /// - Throws: An Error.invalidMnemonic error if the checksum of the input mnemonic phrase does not match the expected checksum.
     public static func toEntropy(_ phrase: [String], wordlist: [String] = Wordlists.english) throws -> [UInt8] {
         let bits = phrase.map { word -> String in
             let index = wordlist.firstIndex(of: word)!
@@ -141,6 +183,15 @@ public class Mnemonic {
         return entropyBytes
     }
 
+    /// Checks if a given mnemonic phrase is valid based on a specified wordlist.
+    ///
+    /// - Parameters:
+    ///    - phrase: A list of mnemonic words to be validated.
+    ///    - wordlist: An optional parameter specifying the wordlist to be used for validation. If not provided, the default English wordlist will be used.
+    ///
+    /// - Returns: A boolean value indicating whether the input mnemonic phrase is valid according to the specified wordlist.
+    ///
+    /// - Note: This function does not check if the mnemonic phrase represents a valid wallet or private key. It only checks the validity of the mnemonic words and their corresponding checksum.
     public static func isValid(phrase: [String], wordlist: [String] = Wordlists.english) -> Bool {
         var bits = ""
         for word in phrase {
@@ -163,6 +214,13 @@ public class Mnemonic {
         return checksumBits == deriveChecksumBits(entropyBytes)
     }
 
+    /// Calculates the checksum bits for a given array of bytes.
+    ///
+    /// - Parameter bytes: An array of bytes for which to calculate the checksum bits.
+    ///
+    /// - Returns: A string representing the checksum bits.
+    ///
+    /// - Note: The length of the checksum bits is determined by the number of bits in the input data. The checksum bits are derived by computing the SHA256 hash of the input data and returning a prefix of the resulting hash of length ENT / 32, where ENT is the number of bits in the input data.
     public static func deriveChecksumBits(_ bytes: [UInt8]) -> String {
         let ENT = bytes.count * 8
         let CS = ENT / 32
@@ -171,7 +229,8 @@ public class Mnemonic {
         let hashbits = String(hash.flatMap { ("00000000" + String($0, radix: 2)).suffix(8) })
         return String(hashbits.prefix(CS))
     }
-    
+
+    /// Used for deriving the seed when generating the private and public keys
     public var seed: Data? {
         let mnemonic = (phrase.joined(separator: " ") as NSString).decomposedStringWithCompatibilityMapping
         let salt = (("mnemonic" + passphrase) as NSString).decomposedStringWithCompatibilityMapping
@@ -189,4 +248,3 @@ extension Mnemonic: Equatable {
         lhs.phrase == rhs.phrase && lhs.passphrase == rhs.passphrase
     }
 }
-
